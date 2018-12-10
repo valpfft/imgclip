ed5b3db596977b3d2e79f003dcdde202320c80bc
Valiantsin Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2018-12-10 19:49:16 +0100

    refactor clipboard_to_file function;

--- ext/imgclip/imgclip.c
+++ ext/imgclip/imgclip.c
@@ -2,46 +2,39 @@
 #include <gtk/gtk.h>
 #include "extconf.h"
 
-int clipboard_to_file(char* filename) {
+static VALUE clipboard_to_file(VALUE _self, VALUE path) {
+    char* c_path = StringValueCStr(path);
+
+    Check_Type(path, T_STRING);
+
     GtkClipboard *clipboard;
     GdkPixbuf *pixbuf;
 
-    /* Consider all images as jpg for now */
+    clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
+    pixbuf = gtk_clipboard_wait_for_image(clipboard);
+
+    if (pixbuf == NULL) {
+        rb_raise(rb_eArgError, "Clipboard content could not be converted into an image.");
+    }
+
+    GError *gerror = NULL;
     char* image_type = "jpeg";
-    int error = 0;
+    gdk_pixbuf_save(pixbuf, c_path, image_type, &gerror, NULL);
 
-    if(error == 0) {
-        clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
-        pixbuf = gtk_clipboard_wait_for_image(clipboard);
+    if (gerror) {
+        g_error_free(gerror);
+        rb_raise(rb_eException, "Unable to save clipboard image to %s: %s\n", c_path, gerror->message);
+    }
 
-        GError *gerror = NULL;
-        gdk_pixbuf_save(pixbuf, filename, image_type, &gerror, NULL);
-
-        if(gerror != NULL) {
-            printf("Unable to save clipboard image to %s: %s\n", filename, gerror->message);
-            error = 1;
-
-            g_error_free(gerror);
-        }
-
+    if (pixbuf != NULL) {
         g_object_unref(pixbuf);
     }
 
-    return error;
-}
-
-VALUE save(VALUE _self, VALUE path) {
-  Check_Type(path, T_STRING);
-
-  int error = 0;
-
-  error = clipboard_to_file(StringValueCStr(path));
-
-  return error;
+    return path;
 }
 
 void Init_imgclip() {
-  gtk_init(NULL, NULL);
-  VALUE mod = rb_define_module("Imgclip");
-  rb_define_module_function(mod, "save", save, 1);
+    gtk_init(NULL, NULL);
+    VALUE mod = rb_define_module("Imgclip");
+    rb_define_module_function(mod, "clipboard_to_file", clipboard_to_file, 1);
 }
